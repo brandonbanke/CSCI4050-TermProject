7a4d62ca576cd48ee66e78d962cf55cfa1f6f875
brandonbanke <55603418+brandonbanke@users.noreply.github.com>
2021-10-27 13:57:02 -0400

    php test added

--- PHP/email-verification.php
+++ PHP/email-verification.php
@@ -2,8 +2,8 @@
     $to = 'bbrandon107@gmail.com';
     $subject = 'Subject!';
     $message = '<p>this is the message of the email<p>';
-    $headers .= "From: Brandon Banke <bhb13727@uga.edu>\r\n";
-    $headers .= "Reply To: bhb1372@uga.edu\r\n";
+    $headers .= "From: Brandon Banke <simran2000@gmail.com>\r\n";
+    $headers .= "Reply To: simran2000@gmail.com\r\n";
     $headers .= "Content-type: text/html\r\n";
     mail($to, $subject, $message, $headers);
 ?>
