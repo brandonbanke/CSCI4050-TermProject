dbe0b489bf091a2f115fc5c52af2d95ce86079ea
brandonbanke <55603418+brandonbanke@users.noreply.github.com>
2021-11-15 09:26:04 -0500

    edit promotion working

--- PHP/editPromotion.php
+++ PHP/editPromotion.php
@@ -1,44 +1,47 @@
 <?php 
     require("../PHP/database.php");
     
-    $promoIden = filter_input(INPUT_POST, 'promotion_id');
+    $promotionIden = filter_input(INPUT_POST, 'promotion_id');
+    
+
 
     $changequery = "SELECT promoName FROM promotion
-                        WHERE id= '$promoIden'
+                        WHERE id= '$promotionIden'
                         ";
     $changestatement = $db->prepare($changequery);
     $changestatement->execute();
     $rowcount = $changestatement->rowCount();
 
     $pNameTitle = filter_input(INPUT_POST, 'new_pName');
-    $changePromoName = "UPDATE promotion
-    SET promoName = '$pNameTitle' WHERE WHERE id= '$promoIden'";
-    $db->exec($changePromoName);
+    $changeNameTitle = "UPDATE promotion
+    SET promoName = '$pNameTitle' WHERE id= '$promotionIden'";
+    $db->exec($changeNameTitle);
 
 
     $changequery2 = "SELECT code FROM promotion
-                        WHERE id= '$promoIden'
+                        WHERE id= '$promotionIden'
                         ";
     $changestatement2 = $db->prepare($changequery2);
     $changestatement2->execute();
     $rowcount2 = $changestatement2->rowCount();
 
-    $pCodes = filter_input(INPUT_POST, 'new_pCode');
-    $changePromoCode = "UPDATE promotion
-    SET code = '$pCodes' WHERE WHERE id= '$promoIden'";
-    $db->exec($changePromoCode);  
-    
+    $pCodeTitle = filter_input(INPUT_POST, 'new_pCode');
+    $changeCodeTitle = "UPDATE promotion
+    SET code = '$pCodeTitle' WHERE id= '$promotionIden'";
+    $db->exec($changeCodeTitle);
+
+
     $changequery3 = "SELECT promDescription FROM promotion
-                        WHERE id= '$promoIden'
+                        WHERE id= '$promotionIden'
                         ";
     $changestatement3 = $db->prepare($changequery3);
     $changestatement3->execute();
     $rowcount3 = $changestatement3->rowCount();
 
-    $pDescriptions = filter_input(INPUT_POST, 'new_pDescription');
-    $changePromoDescription = "UPDATE promotion
-    SET promDescription = '$pDescriptions' WHERE WHERE id= '$promoIden'";
-    $db->exec($changePromoDescription);
+    $pDescr = filter_input(INPUT_POST, 'new_pDescription');
+    $changeDescrip = "UPDATE promotion
+    SET promDescription = '$pDescr' WHERE id= '$promotionIden'";
+    $db->exec($changeDescrip);
 
     include("../HTML/adminMenu.php");
 ?>
